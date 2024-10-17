9eae6cc61c40b1912faf313cfccc1ecaadf287d8
makiwebdeveloper <makiwebdeveloper@gmail.com>
2024-10-17 11:35:59 +0200

    version 1.0

--- js/script.js
+++ js/script.js
@@ -1,18 +1,44 @@
 //debugger;
 
-const textareaElement = document.getElementById("textarea");
-const resultElement = document.getElementById("result");
+const form = document.getElementById("form");
+const formCard = document.getElementById("formCard");
+const resultCard = document.getElementById("resultCard");
+const totalPerPerson = document.getElementById("totalPerPerson");
+const charityElement = document.getElementById("charity");
+const close = document.getElementById("close");
 
-textareaElement.addEventListener("input", wordCount);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const antal = Number(form["antal"].value);
+  const total = Number(form["total"].value);
+  const dricks = Number(form["dricks"].value);
 
-function wordCount() {
-    const words = textareaElement.value.split(/[, \t\r\n]+/g).filter((elmt) => elmt !== "");
+  form.reset();
 
-    if(words.length === 1) {
-        resultElement.innerText = `The text contains ${words.length} word`;
-    } else {
-        resultElement.innerText = `The text contains ${words.length} words`;
-    }
-}
+  const resultTotal = total + dricks;
+  const costPerPersonNotFloored = resultTotal / antal;
 
+  let charity = Math.floor(resultTotal * 0.02);
+
+  if (charity < 1) {
+    charity = 1;
+  }
+
+  const finalCostPerPerson = costPerPersonNotFloored + charity / antal;
+
+  totalPerPerson.innerText = finalCostPerPerson + " kr";
+  charityElement.innerText = charity + " kr";
+
+  resultCard.style.display = "block";
+  formCard.style.display = "none";
+
+  console.log("resultTotal", resultTotal);
+  console.log("charity", charity);
+  console.log("finalCostPerPerson", finalCostPerPerson);
+});
+
+close.addEventListener("click", (event) => {
+  resultCard.style.display = "none";
+  formCard.style.display = "block";
+});
